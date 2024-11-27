77e3b3a2f90c7f87c245bbd6d2629e43c9d98233
María Sol <mikaiha888@gmail.com>
2024-11-27 16:49:58 -0300

    feat(services): create services page
    Implemented the services page with initial layout, styles, and content.

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -44,12 +44,19 @@ const Services = () => {
           {services.map((service, index) => (
             <div key={index} className="flex flex-col justify-center flex-1 gap-6 group">
               <div className="flex items-center justify-between w-full">
-                <div className="text-5xl font-extrabold text-transparent transition-all duration-500 text-outline group-hover:text-outline-hover">{service.num}</div>
-                <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                <div className="text-5xl font-extrabold text-transparent transition-all duration-500 text-outline group-hover:text-outline-hover">
+                  {service.num}
+                </div>
+                <Link
+                  href={service.href}
+                  className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                >
                   <BsArrowDownRight className="text-3xl text-primary" />
                 </Link>
               </div>
-              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all furation-500">{service.title}</h2>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all furation-500">
+                {service.title}
+              </h2>
               <p className="text-white/60">{service.description}</p>
               <div className="w-full border-b border-white/20"></div>
             </div>
